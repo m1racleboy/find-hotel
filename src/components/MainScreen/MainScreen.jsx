b223746df53d2be1a7501ee2c7c398fd7b914e54
Nikita <miracleboy3228@mail.ru>
2021-09-28 18:47:19 +0300

    Сейв перед фичами

--- src/components/MainScreen/MainScreen.jsx
+++ src/components/MainScreen/MainScreen.jsx
@@ -23,11 +23,22 @@ export default function MainScreen() {
               </ul>
               <p className="catalog-hotels__date">07 июля 2020</p>
             </div>
-            <div className="catalog-hotels__slider"></div>
+            <div className="catalog-hotels__slider">
+              <ul className="catalog-hotels__slide-list">
+                <li className="catalog-hotels__slide-item"><img className="catalog-hotels__slide-img" src="img/img-2.png" width="164" height="149" alt="img2" /></li>
+                <li className="catalog-hotels__slide-item"><img className="catalog-hotels__slide-img" src="img/img-3.png" width="164" height="149" alt="igm3" /></li>
+                <li className="catalog-hotels__slide-item"><img className="catalog-hotels__slide-img" src="img/img-1.png" width="164" height="149" alt="img1" /></li>
+                <li className="catalog-hotels__slide-item"><img className="catalog-hotels__slide-img" src="img/img-4.png" width="164" height="149" alt="img4" /></li>
+              </ul>
+            </div>
             <h3 className="catalog-hotels__favorite-count">Добавлено в Избранное: <span className="catalog-hotels__favorite-count--value">{favoriteHotels}</span> {favoriteHotels === 1 ? 'отель' : favoriteHotels < 5 ? 'отеля' : 'отелей'}</h3>
             <ul className="catalog-hotels__list">
-              <HotelCard withHome={true}/>
-              <HotelCard withHome={true}/>
+              <HotelCard withHome={true} />
+              <HotelCard withHome={true} />
+              <HotelCard withHome={true} />
+              <HotelCard withHome={true} />
+              <HotelCard withHome={true} />
+              <HotelCard withHome={true} />
             </ul>
           </section>
         </div>
